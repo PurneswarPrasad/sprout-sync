2337e2baae275cb96fd55c7867a732af2a4c2b1d
purnez <prasad.sonu786@gmail.com>
2025-08-29 18:54:01 +0530

    updating sameSite

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -69,7 +69,7 @@ app.use(session({
     secure: process.env['NODE_ENV'] === 'production',
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    sameSite: process.env['NODE_ENV'] === 'production' ? 'strict' : 'lax',
+    sameSite: process.env['NODE_ENV'] === 'production' ? 'none' : 'lax',
   },
 }));
 
