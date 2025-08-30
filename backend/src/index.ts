70acccca65955489210ca3f181fcb9770e75b5f7
purnez <prasad.sonu786@gmail.com>
2025-08-30 13:25:27 +0530

    chnaged cors from array to choice

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -35,10 +35,7 @@ app.use(compression());
 // CORS configuration
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      `${process.env['CORS_ORIGIN']}`,
-    ],
+    origin: process.env['CORS_ORIGIN'] || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
